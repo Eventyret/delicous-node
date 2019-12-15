be1e14491a4ad1f45bf21c4d794213f6ebeb6eac
Eventyret <simen.dehlin@gmail.com>
2019-12-15 18:29:24 +0000

    Added webpack optimization

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  // res.send('Hey! It works!');
+  res.render("hello");
 });
 
 module.exports = router;
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@
   Okay folks, want to learn a little bit about webpack?
 */
 
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const autoprefixer = require('autoprefixer');
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+const TerserPlugin = require("terser-webpack-plugin");
 
 /*
   webpack sees every file as a module.
@@ -17,10 +17,12 @@ const TerserPlugin = require('terser-webpack-plugin');
 // This is our JavaScript rule that specifies what to do with .js files
 const javascript = {
   test: /\.(js)$/, // see how we match anything that ends in `.js`? Cool
-  use: [{
-    loader: 'babel-loader',
-    options: { presets: ['env'] } // this is one way of passing options
-  }],
+  use: [
+    {
+      loader: "babel-loader",
+      options: { presets: ["env"] } // this is one way of passing options
+    }
+  ]
 };
 
 /*
@@ -28,9 +30,11 @@ const javascript = {
 */
 
 const postcss = {
-  loader: 'postcss-loader',
+  loader: "postcss-loader",
   options: {
-    plugins() { return [autoprefixer({ browsers: 'last 3 versions' })]; }
+    plugins() {
+      return [autoprefixer({ browsers: "last 3 versions" })];
+    }
   }
 };
 
@@ -40,44 +44,47 @@ const styles = {
   // here we pass the options as query params b/c it's short.
   // remember above we used an object for each loader instead of just a string?
   // We don't just pass an array of loaders, we run them through the extract plugin so they can be outputted to their own .css file
-  use: ExtractTextPlugin.extract(['css-loader?sourceMap', postcss, 'sass-loader?sourceMap'])
+  use: ExtractTextPlugin.extract([
+    "css-loader?sourceMap",
+    postcss,
+    "sass-loader?sourceMap"
+  ])
 };
 
 // We can also use plugins - this one will compress the crap out of our JS
-
 
 // OK - now it's time to put it all together
 const config = {
   entry: {
     // we only have 1 entry, but I've set it up for multiple in the future
-    App: './public/javascripts/delicious-app.js'
+    App: "./public/javascripts/delicious-app.js"
   },
   // we're using sourcemaps and here is where we specify which kind of sourcemap to use
-  devtool: 'source-map',
+  devtool: "source-map",
   // Once things are done, we kick it out to a file.
   output: {
     // path is a built in node module
     // __dirname is a variable from node that gives us the
-    path: path.resolve(__dirname, 'public', 'dist'),
+    path: path.resolve(__dirname, "public", "dist"),
     // we can use "substitutions" in file names like [name] and [hash]
     // name will be `App` because that is what we used above in our entry
-    filename: '[name].bundle.js'
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-    },
+    filename: "[name].bundle.js"
   },
 
   // remember we said webpack sees everthing as modules and how different loaders are responsible for different file types? Here is is where we implement them. Pass it the rules for our JS and our styles
   module: {
-    rules: [javascript, styles]
+    rules: [javascript, styles],
   },
   // finally we pass it an array of our plugins - uncomment if you want to uglify
   // plugins: [uglify]
   plugins: [
     // here is where we tell it to output our css to a separate file
-    new ExtractTextPlugin('style.css'),
-  ]
+    new ExtractTextPlugin("style.css")
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  }
 };
 // webpack is cranky about some packages using a soon to be deprecated API. shhhhhhh
 process.noDeprecation = true;
