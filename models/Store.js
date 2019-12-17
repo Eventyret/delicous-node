cf79de995f2593462c474d3a377db8dc402d2921
Eventyret <simen.dehlin@gmail.com>
2019-12-17 20:05:32 +0000

    Exported storeSchema

--- models/Store.js
+++ models/Store.js
@@ -1,3 +1,9 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
+const slug = require("slugs");
 
+const storeSchema = new mongoose.Schema({
+	
+});
+
+module.exports = mongoose.model("Store", storeSchema);
