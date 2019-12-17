15e716542e1f6ce1802c0d188d0c5d00b5b08673
Eventyret <simen.dehlin@gmail.com>
2019-12-17 20:08:52 +0000

    Added first schema

--- models/Store.js
+++ models/Store.js
@@ -3,7 +3,17 @@ mongoose.Promise = global.Promise;
 const slug = require("slugs");
 
 const storeSchema = new mongoose.Schema({
-	
+  name: {
+    type: String,
+    trim: true,
+    required: "Please enter a store name!"
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
 });
 
 module.exports = mongoose.model("Store", storeSchema);
