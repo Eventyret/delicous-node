793633ab4ffeae78690bbad979efb78185876bac
Eventyret <simen.dehlin@gmail.com>
2019-12-28 16:25:29 +0000

    Added addStore route

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const storeController = require("../controllers/storeController");
 // Do work here
 router.get("/", storeController.homePage);
+router.get("/add", storeController.addStore);
 
 module.exports = router;
