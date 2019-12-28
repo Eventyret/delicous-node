cee307d788d90c63d77fbe212f30a49d2400e829
Eventyret <simen.dehlin@gmail.com>
2019-12-28 18:01:23 +0000

    Created method for creating Store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -5,3 +5,7 @@ exports.homePage = (req, res) => {
 exports.addStore = (req, res) => {
   res.render("editStore", { title: "Add Store" });
 };
+
+exports.createStore = (req, res) => {
+  res.json(req.body)
+}
