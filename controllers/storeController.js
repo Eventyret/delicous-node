e0bfa1bdc32ae711407e982b478ef2eb30b16b55
Eventyret <simen.dehlin@gmail.com>
2020-01-01 13:34:48 +0000

    Added getStores method and route

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -16,3 +16,7 @@ exports.createStore = async (req, res) => {
   );
   res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = (req, res) => {
+  res.render("stores", { title: "Stores" });
+};
