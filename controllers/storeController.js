226f32efd4c6960d5ec7d518f7baecaf1d295283
Eventyret <simen.dehlin@gmail.com>
2020-01-01 20:43:15 +0000

    Added method to update the stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -24,6 +24,17 @@ exports.getStores = async (req, res) => {
 
 exports.editStore = async (req, res) => {
   const store = await Store.findOne({ _id: req.params.id });
-  res.json(store);
   res.render("editStore", { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true
+  }).exec();
+  req.flash(
+    "success",
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store ➡</a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
 };
