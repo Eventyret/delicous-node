44dc8c1cfbbe589466dd3c66f778f2762ce06367
Eventyret <simen.dehlin@gmail.com>
2020-01-02 20:26:08 +0000

    Fixed where updating the store did not update the type of pointer

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -28,6 +28,7 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  req.body.location.type = "Point";
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true,
     runValidators: true
